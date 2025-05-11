c46fe523eb26d4754caf9faa230276f2b83597ad
Umamaheswara Madasu <57607368+uma26madasu@users.noreply.github.com>
2025-05-11 00:17:14 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,8 +1,6 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Optional: if you're using Tailwind or global styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
