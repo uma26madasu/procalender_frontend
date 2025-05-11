6e7a49f6ec51508a1b93b2febeb0f7957d11a2e9
Umamaheswara Madasu <57607368+uma26madasu@users.noreply.github.com>
2025-05-11 11:27:43 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       '/api': {
         target: 'https://procalender-backend.onrender.com',
         changeOrigin: true,
-        secure: false
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
