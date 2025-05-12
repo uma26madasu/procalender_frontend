422a7790efda8eaf94ddc04726c2fd0900c1b00b
uma26madasu <57607368+uma26madasu@users.noreply.github.com>
2025-05-11 22:11:25 -0400

    updated files

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp, getApps } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
