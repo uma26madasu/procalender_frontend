cb20c311ac7d8498916255ffb8bdad80ddd3ddca
uma26madasu <57607368+uma26madasu@users.noreply.github.com>
2025-05-11 21:21:45 -0400

    new changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, useLocation } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+const RouterDebugger = () => {
+  const location = useLocation();
+  
+  useEffect(() => {
+    console.log('Route changed to:', location.pathname);
+  }, [location]);
+
+  return null;
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/"> {/* Added explicit basename */}
+    <BrowserRouter basename="/">
+      <RouterDebugger />
       <App />
     </BrowserRouter>
   </React.StrictMode>
