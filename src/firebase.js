5b73e4c08e5c68661f0a948899db250f39252474
uma26madasu <57607368+uma26madasu@users.noreply.github.com>
2025-05-11 22:48:31 -0400

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDngn-nNfWDWGjXcDdtO6GiM5xOBQvYil8",
-  authDomain: "pro-calender-uma".firebaseapp.com,
+  authDomain: "pro-calender-uma.firebaseapp.com",
   projectId: "pro-calender-uma",
   // Add other Firebase config keys from your Firebase Console
 };
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -9,7 +9,7 @@ import logo from '/logo.svg'; // Standard SVG import
 // Firebase Configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDngn-nNfWDWGjXcDdtO6GiM5xOBQvYil8",
-  authDomain: "pro-calender-uma".firebaseapp.com,
+  authDomain: "pro-calender-uma.firebaseapp.com",
   projectId: "pro-calender-uma",
   // Add other configs from Firebase Console
 };
