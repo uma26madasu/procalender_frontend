4d3030062c5edb68c7eb0a4d154929f0b431bd98
uma26madasu <ummahesh90@gmail.com>
2025-05-17 22:59:25 -0400

    updated SlotifyLogo

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { auth } from '../../firebase';
-import SlotifyLogo from '../../components/SlotifyLogo';
+import SlotifyLogo from '../SlotifyLogo';
 
 const Sidebar = () => {
   const navigate = useNavigate();
